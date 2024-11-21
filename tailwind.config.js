94665a64cf5e8ce43ccf72704229e0335284f7f5
Joeri Harreman <harr0006@hz.nl>
2024-11-21 15:08:41 +0100

    inital commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        greenLight: "#C1E899",
+        brownWarm: "#9A6735",
+        greenPale: "#E6F0DC",
+        greenDeep: "#55883B",
+      },
+    },
   },
   plugins: [],
 }
