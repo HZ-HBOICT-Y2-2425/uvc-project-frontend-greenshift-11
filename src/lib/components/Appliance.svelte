a56d90126e0bcba78b205ebd8b2f76d6362d88a7
zmyslek <zuzia.myslek@gmail.com>
2024-11-28 15:32:12 +0100

    aved temporary

---
+++ src/lib/components/Appliance.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { getContext, onMount } from 'svelte';
+  import { appliances } from '$lib/stores/selectedAppliancesStore';
+    import { get } from 'svelte/store';
+  export let applianceUrl;
+
+  let appliance = [];
+  let loading = true;
+  let error = null;
+
+  const apiUrl = `${getContext('apiReference').mainUrl}${applianceUrl}`;
+
+  onMount(async () => {
+    
+  })
+</script>
