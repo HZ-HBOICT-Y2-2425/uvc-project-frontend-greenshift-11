f4fb2523d7bee3affd13c9a912e2141c612b9aaf
zmyslek <zuzia.myslek@gmail.com>
2024-12-04 11:48:37 +0100

    fixing the pipeline vol 2

--- __tests__/page.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/svelte";
-import Component from "../src/routes/+page.svelte";
-
-test("it works", async () => {
-  render(Component);
-  const welcomeText = await screen.findByText("Welcome");
-  expect(welcomeText).toBeInTheDocument();
-});
